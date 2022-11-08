782bdad539ae7949a60b79c212ff1a2ccae1aab6
Bryan Richards <brichardssa@gmail.com>
2022-11-08 13:33:16 +0200

    initial system

---
+++ DataEncryptionUtil.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsDemo
+{
+    partial class DataEncryptionUtil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.progressBarEncrypt = new System.Windows.Forms.ProgressBar();
+            this.labelEncryptProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(12, 12);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(178, 40);
+            this.buttonEncrypt.TabIndex = 0;
+            this.buttonEncrypt.Text = "Encrypt Data";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // progressBarEncrypt
+            // 
+            this.progressBarEncrypt.Location = new System.Drawing.Point(196, 12);
+            this.progressBarEncrypt.Name = "progressBarEncrypt";
+            this.progressBarEncrypt.Size = new System.Drawing.Size(326, 40);
+            this.progressBarEncrypt.TabIndex = 1;
+            // 
+            // labelEncryptProgress
+            // 
+            this.labelEncryptProgress.AutoSize = true;
+            this.labelEncryptProgress.Location = new System.Drawing.Point(313, 64);
+            this.labelEncryptProgress.Name = "labelEncryptProgress";
+            this.labelEncryptProgress.Size = new System.Drawing.Size(0, 13);
+            this.labelEncryptProgress.TabIndex = 2;
+            // 
+            // DataEncryptionUtil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 88);
+            this.Controls.Add(this.labelEncryptProgress);
+            this.Controls.Add(this.progressBarEncrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Name = "DataEncryptionUtil";
+            this.Text = "Data Encryption Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.ProgressBar progressBarEncrypt;
+        private System.Windows.Forms.Label labelEncryptProgress;
+    }
+}
+
---
+++ DataEncryptionUtil.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemo
+{
+    public partial class DataEncryptionUtil : Form
+    {
+        private const string _passPhrase = "Rv4Y0FlLcwi15Ho8L9YBDemoflngu3gx0W15eLYgZhjSOBvYsSBbrPppqpyA";
+        private const string _initVector = "eFmgMM7qykTngG2Q";
+        private const string _hashAlgorithm = "SHA1";
+        private const int _passwordIterations = 2;
+        private const int _keySize = 256;
+
+        public DataEncryptionUtil()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            var openFileTextFile = new OpenFileDialog();
+            openFileTextFile.Filter = "Text Files|*.txt";
+            openFileTextFile.ShowDialog();
+            var fileContents = File.ReadAllText(openFileTextFile.FileName).Split("\r\n".ToCharArray());
+            var encryptedData = "";
+            for (var i = 0; i < fileContents.Length; i++)
+            {
+                var line = fileContents[i].Trim();
+                var salt = CreateSalt();
+                var encryptedRecord = Encrypt(line, salt);
+                if (encryptedData.Length > 0)
+                {
+                    encryptedData += "\r\n";
+                }
+                encryptedData += encryptedRecord + "\t" + salt + "\r\n";
+                var progressPercent = Convert.ToInt32((Convert.ToDecimal(i) / fileContents.Length) * 100M);
+                progressBarEncrypt.Value = progressPercent;
+                labelEncryptProgress.Text = i.ToString() + " (" + progressPercent.ToString() + "%)";
+            }
+            var saveTextFile = new SaveFileDialog();
+            saveTextFile.Filter = "Text File|*.txt";
+            saveTextFile.ShowDialog();
+            var encryptedFile = File.CreateText(saveTextFile.FileName);
+            encryptedFile.Write(encryptedData);
+            encryptedFile.Flush();
+            encryptedFile.Close();
+        }
+
+        public string Encrypt(string plainText, string saltValue)
+        {
+            var initVectorBytes = Encoding.ASCII.GetBytes(_initVector);
+            var saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            var password = new PasswordDeriveBytes(_passPhrase, saltValueBytes, _hashAlgorithm, _passwordIterations);
+            var keyBytes = password.GetBytes(_keySize / 8);
+            var symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
+            var memoryStream = new MemoryStream();
+            var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            var cipherTextBytes = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            var cipherText = Convert.ToBase64String(cipherTextBytes);
+            Thread.Sleep(50);
+            return cipherText;
+        }
+
+        public string Decrypt(string cipherText, string saltValue)
+        {
+            if (string.IsNullOrWhiteSpace(cipherText))
+            {
+                return string.Empty;
+            }
+            var initVectorBytes = Encoding.ASCII.GetBytes(_initVector);
+            var saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+            var cipherTextBytes = Convert.FromBase64String(cipherText);
+            var password = new PasswordDeriveBytes(_passPhrase, saltValueBytes, _hashAlgorithm, _passwordIterations);
+            var keyBytes = password.GetBytes(_keySize / 8);
+            var symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            var plainTextBytes = new byte[cipherTextBytes.Length];
+            var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            var plainText = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            return plainText;
+        }
+
+        public string CreateSalt()
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var buff = new byte[32];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+    }
+}
